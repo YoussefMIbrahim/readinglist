b3c51ecb19a5b4789da95c180a02ee4cf16efa02
name <me@gmail.com>
2020-09-18 15:26:58 -0500

    Checks for pre-existing book within the list, does not allow duplicates and prompts user if they try to add the same book twice

--- main.py
+++ main.py
@@ -32,8 +32,12 @@ def create_menu():
 
 
 def add_book():
-    new_book = ui.get_book_info()
-    new_book.save()
+    try:
+        new_book = ui.get_book_info()
+        new_book.save()
+    except Exception:
+        print("You cannot add the same book twice to the list.")
+
     
 
 def show_read_books():
